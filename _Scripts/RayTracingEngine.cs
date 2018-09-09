20e0264b1dfc9c6ffce5c2f0286661f8fe2970bb
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2018-09-09 09:35:05 +0900

    AA added

--- _Scripts/RayTracingEngine.cs
+++ _Scripts/RayTracingEngine.cs
@@ -5,23 +5,34 @@ public class RayTracingEngine {
 	public Color[,] Pixel {
 		get { return pixel_; }
 	}
+	public int aaQuality = 100;
 	private int width_, height_;
 	private Color[,] pixel_;
 	private CameraRay cameraRay_;
+	private System.Random random_;
 	public RayTracingEngine(int width, int height) {
 		width_ = width;
 		height_ = height;
 		cameraRay_ = new CameraRay(width_, height_);
 		pixel_ = new Color[width_, height_];
+		random_ = new System.Random();
 	}
 	public void Update(Scene scene) {
 		HitRecord hit = new HitRecord();
 		for (int y = height_ - 1; y >= 0; --y) {
 			for (int x = 0; x < width_; ++x) {
-				float u = (float)x / (float)width_;
-				float v = (float)y / (float)height_;
-				pixel_[x, y] = scene.GetColor(cameraRay_.GetRay(u, v), hit);
+				Color color = Color.black;
+				for (int s = 0; s < aaQuality; ++s) {
+					float u = ((float)x + UnitRandFloat()) / (float)width_;
+					float v = ((float)y + UnitRandFloat()) / (float)height_;
+					color += scene.GetColor(cameraRay_.GetRay(u, v), hit);
+				}
+				pixel_[x, y] = color / aaQuality;
 			}
 		}
+	}
+	public float UnitRandFloat() {
+		var r = random_.NextDouble();
+		return (float)r;
 	}
 }

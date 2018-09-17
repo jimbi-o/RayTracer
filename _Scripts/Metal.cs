5aa8104abc403c166e19828ceba10f233e85b02b
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2018-09-17 21:06:13 +0900

    metal fuzziness introduced

--- _Scripts/Metal.cs
+++ _Scripts/Metal.cs
@@ -6,12 +6,18 @@ public class Metal : MaterialData {
 		get { return albedo_; }
 		set { albedo_ = value; }
 	}
+	public float Fuzz {
+		get { return fuzz_; }
+		set { fuzz_ = Mathf.Min(value, 1.0f); }
+	}
 	private Vector3 albedo_;
-	public Metal(Vector3 a) {
+	private float fuzz_;
+	public Metal(Vector3 a, float fuzz) {
 		Albedo = a;
+		Fuzz = fuzz;
 	}
 	public override bool Scatter(Ray ray, HitRecord hit) {
-		hit.scatteredRay = new Ray(hit.p, Vector3.Reflect(ray.Direction, hit.normal));
+		hit.scatteredRay = new Ray(hit.p, Vector3.Reflect(ray.Direction, hit.normal) + Fuzz * Util.RandomInUnitSphere());
 		hit.attenuation = Albedo;
 		return Vector3.Dot(hit.scatteredRay.Direction, hit.normal) > 0.0f;
 	}

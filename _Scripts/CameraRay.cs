72f48a77418f543ab4a9808328d26ada1b0e506d
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2018-09-22 16:16:17 +0900

    DoF implemented

--- _Scripts/CameraRay.cs
+++ _Scripts/CameraRay.cs
@@ -7,28 +7,36 @@ public class CameraParam {
 	public Vector3 vup;
 	public float fov;
 	public float aspect;
+	public float fstop;
 	public CameraParam() {
 		vup.Set(0.0f, 1.0f, 0.0f);
 		fov = 45.0f;
+		fstop = 1.0f / 4.0f;
 	}
 }
 public class CameraRay {
+	private Vector3 u_, v_, w_;
 	private Vector3 horizontal_, vertical_, origin_;
 	private Vector3 lowerLeftCorner_;
+	private float lensRadius_;
 	public CameraRay(CameraParam param) {
-		float theta = param.fov * Mathf.PI / 180.0f;
-		float halfHeight = Mathf.Tan(theta / 2.0f);
-		float halfWidth = param.aspect * halfHeight;
+		lensRadius_ = 0.5f / param.fstop;
+		var theta = param.fov * Mathf.PI / 180.0f;
+		var halfHeight = Mathf.Tan(theta / 2.0f);
+		var halfWidth = param.aspect * halfHeight;
 		origin_ = param.lookFrom;
-		var w = (param.lookFrom - param.lookAt).normalized;
-		var u = Vector3.Cross(param.vup, w).normalized;
-		var v = Vector3.Cross(w, u);
-		lowerLeftCorner_ = origin_ - halfWidth * u - halfHeight * v - w;
-		horizontal_ = 2.0f * halfWidth * u;
-		vertical_ = 2.0f * halfHeight * v;
+		w_ = (param.lookFrom - param.lookAt).normalized;
+		u_ = Vector3.Cross(param.vup, w_).normalized;
+		v_ = Vector3.Cross(w_, u_);
+		var focusDistance = (param.lookFrom - param.lookAt).magnitude;
+		lowerLeftCorner_ = origin_ - focusDistance * (halfWidth * u_ + halfHeight * v_ + w_);
+		horizontal_ = 2.0f * halfWidth * u_ * focusDistance;
+		vertical_ = 2.0f * halfHeight * v_ * focusDistance;
 	}
-	public Ray GetRay(float u, float v) {
-		var ray = new Ray(origin_, lowerLeftCorner_ + u * horizontal_ + v * vertical_ - origin_);
+	public Ray GetRay(float s, float t) {
+		var rd = lensRadius_ * Util.RandomInUnitSphere();
+		var offset = u_ * rd.x + v_ * rd.y;
+		var ray = new Ray(origin_ + offset, lowerLeftCorner_ + s * horizontal_ + t * vertical_ - origin_ - offset);
 		return ray;
 	}
 }

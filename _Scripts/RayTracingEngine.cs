6e217b550b8492d7e9deeb820ddadec2896f1b6a
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2018-09-22 18:15:29 +0900

    preview pixel one by one

--- _Scripts/RayTracingEngine.cs
+++ _Scripts/RayTracingEngine.cs
@@ -10,6 +10,7 @@ public class RayTracingEngine {
 	private Color[,] pixel_;
 	private CameraRay cameraRay_;
 	private int sampleCount_;
+	private int x_, y_;
 	public RayTracingEngine(int quality, int width, int height) {
 		maxSampleNum_ = quality;
 		width_ = width;
@@ -28,18 +29,24 @@ public class RayTracingEngine {
 				pixel_[x, y] = Color.black;
 			}
 		}
+		x_ = width_;
+		y_ = 0;
 	}
 	public void Update(Scene scene) {
 		if (sampleCount_ >= maxSampleNum_) return;
-		++sampleCount_;
-		var alpha = 1.0f / sampleCount_;
-		for (int y = height_ - 1; y >= 0; --y) {
-			for (int x = 0; x < width_; ++x) {
-				float u = ((float)x + Util.UnitRandFloat()) / (float)width_;
-				float v = ((float)y + Util.UnitRandFloat()) / (float)height_;
-				var color = Util.ConvertToColor(scene.GetColor(cameraRay_.GetRay(u, v)));
-				pixel_[x, y] = Color.Lerp(pixel_[x, y], color, alpha);
+		++x_;
+		if (x_ >= width_) {
+			x_ = 0;
+			--y_;
+			if (y_ < 0) {
+				y_ = height_ - 1;
+				++sampleCount_;
 			}
 		}
+		var alpha = 1.0f / sampleCount_;
+		float u = ((float)x_ + Util.UnitRandFloat()) / (float)width_;
+		float v = ((float)y_ + Util.UnitRandFloat()) / (float)height_;
+		var color = Util.ConvertToColor(scene.GetColor(cameraRay_.GetRay(u, v)));
+		pixel_[x_, y_] = Color.Lerp(pixel_[x_, y_], color, alpha);
 	}
 }
